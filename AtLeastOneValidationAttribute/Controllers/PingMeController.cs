a7df6f9f086983c17977a2e2b0053508b9a3baf3
Anand Rajput <anandrajput007@gmail.com>
2020-07-05 16:57:55 +0530

    Project Setup for atleast one validation attribute

---
+++ AtLeastOneValidationAttribute/Controllers/PingMeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AtLeastOneValidationAttribute.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PingMeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "You are connected...";
+        }
+    }
+}
---
+++ AtLeastOneValidationAttribute/Models/ContactModel.cs
@@ -0,0 +1,18 @@
+﻿using AtLeastOneValidationAttribute.Util;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AtLeastOneValidationAttribute.Models
+{
+    public class ContactModel
+    {
+        [Required(ErrorMessage = "Full Name is Required.")]
+        public string FullName { get; set; }
+
+        [AtLeastOneRequiredAttribute("UserName, EmailID, MobileNo", ErrorMessage = "At Leaset one required of UserName/EmailID/MobileNo")]
+        public string UserName { get; set; }
+        public string EmailID { get; set; }
+        public string MobileNo { get; set; }
+        public DateTime DOB { get; set; }
+    }
+}
